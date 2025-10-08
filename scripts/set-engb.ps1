75881457b13fa6c80aaf387ed4a4b63b0c6f9a28
andrew-walles-harbottle <149791976+andrew-walles-harbottle@users.noreply.github.com>
2025-10-08 16:04:59 +0100

    Update set-engb.ps1

--- scripts/set-engb.ps1
+++ scripts/set-engb.ps1
@@ -1,5 +1,6 @@
-# Set the system locale to English (United Kingdom)
-Set-WinSystemLocale en-GB
+######
+## Using AVD UK settings does not change the following data 
+######
 
 # Set the culture for the current session to English (United Kingdom)
 Set-Culture -CultureInfo en-GB
@@ -7,12 +8,13 @@ Set-Culture -CultureInfo en-GB
 # Set the home location to UK (GeoId 242)
 Set-WinHomeLocation -GeoId 242
 
-# Repeat setting the culture to ensure it is applied
-Set-Culture en-GB
-
 # Copy the current user's international settings (language, region, input preferences) 
 # to the Welcome screen, system accounts, and all new user accounts
 Copy-UserInternationalSettingsToSystem -WelcomeScreen $true -NewUser $true
 
 # Output confirmation message to indicate the script has completed
 Write-Output "Language and region settings copied to Welcome screen, system accounts and new user accounts."
+
+#########
+## END ##
+#########
